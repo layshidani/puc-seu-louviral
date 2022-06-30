adb36308e8bbf40103adf210e0c7a02527d13560
Lays Hidani <lays.hidani@gmail.com>
2022-06-30 10:08:59 -0300

    style: refactor style

--- seu_lourival/lib/core/theme/text_size.dart
+++ seu_lourival/lib/core/theme/text_size.dart
@@ -1,11 +1,12 @@
 class TextSize {
-  static const xs = 12.0;
-  static const sm = 14.0;
-  static const base = 16.0;
-  static const lg = 18.0;
-  static const xl = 20.0;
-  static const xl2 = 22.0;
-  static const xl3 = 24.0;
+  static const xxs = 12.0;
+  static const xs = 14.0;
+  static const sm = 16.0;
+  static const base = 18.0;
+  static const lg = 20.0;
+  static const xl = 22.0;
+  static const xl2 = 24.0;
+  static const xl3 = 26.0;
   static const xl4 = 28.0;
   static const xl5 = 30.0;
   static const xl6 = 34.0;
