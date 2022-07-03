6c0665a0a21b70b93b116d4e4a83a36ec58d72e7
Lays Hidani <lays.hidani@gmail.com>
2022-07-03 17:53:47 -0300

    style: change ds colors

--- seu_lourival/lib/core/values/colors.dart
+++ seu_lourival/lib/core/values/colors.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class DSColors {
-  static const primary = Color.fromRGBO(45, 84, 116, 1);
-  static const primaryDark = Color.fromARGB(255, 25, 30, 40);
+  static const primary = Color.fromARGB(255, 0, 155, 198);
+  static const primaryDark = Color.fromARGB(255, 2, 72, 91);
+  // static const primary = Color.fromRGBO(45, 84, 116, 1);
+  // static const primaryDark = Color.fromARGB(255, 25, 30, 40);
 }
