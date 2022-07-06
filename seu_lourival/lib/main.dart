42c59aac4c049e74b302c5fd8a02e37745b5d95e
Lays Hidani <lays.hidani@gmail.com>
2022-07-06 20:37:49 -0300

    chore: add firebase

--- seu_lourival/lib/main.dart
+++ seu_lourival/lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
-import 'package:seu_lourival/_debug/page.dart';
 import 'package:seu_lourival/core/values/colors.dart';
+import 'package:seu_lourival/firebase_options.dart';
 import 'package:seu_lourival/routes/pages.dart';
 import 'package:seu_lourival/routes/routes.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
